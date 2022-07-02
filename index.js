9792f5cab2f75bf322df1b0f1c2a1957897df428
mekb the turtle <mekbgithub@icloud.com>
2022-07-02 12:17:32 +1000

    minify each entry to save space but still make editing url or adding more easier

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const axios = require("axios");
 const { XMLParser, XMLBuilder, XMLValidator } = require("fast-xml-parser");
 const fs = require("fs");
-const listFile = "./list.json";
+const path = require("path");
+const listFile = path.join(__dirname, "list.json");
+console.log("reading", listFile);
 const list = JSON.parse(fs.readFileSync(listFile));
 const parser = new XMLParser();
 const listEntries = {};
+const newEntries = {};
 let total = 0;
 let done = 0;
 for (let i in list) {
@@ -13,17 +16,30 @@ for (let i in list) {
 		for (j in list[i].entries) {
 			listEntries[i].push(JSON.stringify(list[i].entries[j]));
 		}
+	} else {
+		newEntries[i] = true;
 	}
 	++total;
 }
 const finish = () => {
 	++done;
 	if (done >= total) {
-		fs.writeFileSync(listFile, JSON.stringify(list, null, "\t"));
+		console.log("writing", listFile);
+		let z = JSON.stringify(list, null, "\t");
+		z = z.replace(/\t{3}\{\n(\t{3}[^\{].*\n)+\t{3}\}/g, e => {
+			e = JSON.stringify(JSON.parse(e));
+			return "\t\t\t" + e;
+		});
+		fs.writeFileSync(listFile, z);
+		process.exit(0);
 	}
 }
 for (let i in list) {
-	if (!list[i].url) continue;
+	if (!list[i].url) {
+		console.error("not a url", list[i].url);
+		continue;
+	}
+	console.log("fetching", list[i].url);
 	axios({
 		url: list[i].url,
 		method: "get",
@@ -40,13 +56,15 @@ for (let i in list) {
 		for (let j in entries) {
 			let a = JSON.stringify(entries[j]);
 			list[i].entries.push(JSON.parse(a));
-			if (!listEntries[i].includes(a)) {
-				console.log(a);
+			if (!newEntries[i]) {
+				if (!listEntries[i].includes(a)) {
+					console.log(a);
+				}
 			}
 		}
 		finish();
 	}).catch(err => {
-		console.error("failed", list[i], err);
+		console.error("failed", list[i].url, err);
 		finish();
 	});
 }
